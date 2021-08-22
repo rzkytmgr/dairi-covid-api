0b4ac6a415e05cfed2edabdde32b57f9983589b6
rzkytmgr <rzkytmgr@gmail.com>
2021-08-22 15:27:46 +0700

    add/fix middleware (data caching) 🛠

--- server/middleware.js
+++ server/middleware.js
@@ -1,5 +1,15 @@
-const cache = new Set();
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
 module.exports = (req, res, next) => {
-	const key = req.originalUrl | req.url;
+	const key = req.originalUrl || req.url;
+	if (cache.get(key)) {
+		return res.send(cache.get(key));
+	} else {
+		res.bodyResponse = res.send;
+		res.send = (body) => {
+			cache.set(key, body, 60 * 60 * 12);
+			res.bodyResponse(body);
+		};
+	}
 	next();
 };
