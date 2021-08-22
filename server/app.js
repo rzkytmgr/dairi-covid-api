39c29ec9934c3f91a0a5d0a19ac584161182fc20
rzkytmgr <rzkytmgr@gmail.com>
2021-08-23 01:24:25 +0700

    export the app :bell:

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 
-const IndexRouter = require("./routes/IndexRouter");
-const DataRouter = require("./routes/DataRouter");
+const IndexRouter = require('./routes/IndexRouter');
+const DataRouter = require('./routes/DataRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use("/data", DataRouter);
-app.use("/", IndexRouter);
+app.use('/data', DataRouter);
+app.use('/', IndexRouter);
 
 app.listen(PORT, function () {
 	console.clear();
-	console.log("Running server port", PORT);
+	console.log('Running server port', PORT);
 });
+
+module.exports = app;
