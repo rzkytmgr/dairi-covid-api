39066360136e6b8a8da6bf8975a878b754c76474
rzkytmgr <rzkytmgr@gmail.com>
2021-08-27 07:18:39 +0700

    🧶 add routes '/kecamatan/list' - show all list of region

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,7 @@
-const data = require("../data/data.json");
+const data = require('../data/data.json');
 
-const { getById, getByDate } = require("../util/function");
-const { success, error, introduction } = require("../util/default");
+const { getById, getByDate } = require('../util/function');
+const { success, error, introduction } = require('../util/default');
 
 const HomeController = (req, res) => {
 	return res.status(200).json({
@@ -20,7 +20,7 @@ const DataController = (req, res) => {
 	return res.status(200).json({
 		...success,
 		data: (function () {
-			return data.map((res) => {
+			return data.map(res => {
 				const { kecamatan, ...result } = res;
 				return result;
 			});
@@ -31,7 +31,7 @@ const DataController = (req, res) => {
 const DataKecamatanController = (req, res) => {
 	const { identifier } = req.params;
 
-	if (typeof identifier === "undefined") {
+	if (typeof identifier === 'undefined') {
 		return res.status(200).json({
 			...success,
 			data: data,
@@ -43,16 +43,28 @@ const DataKecamatanController = (req, res) => {
 	});
 };
 
+const ListKecamatanController = (req, res) => {
+	const final = data[0]['kecamatan'].map(result => {
+		const { data, ...destruct } = result;
+		return destruct;
+	});
+
+	return res.status(200).json({
+		...success,
+		data: final,
+	});
+};
+
 const CustomIdData = (req, res) => {
 	const { identifier, kecamatan } = req.params;
 	if (!isNaN(identifier)) {
 		const dataId = getById(identifier, kecamatan);
-		return typeof dataId === "undefined" ? res.status(404).json({ ...error, message: "Data not found!" }) : res.status(200).json({ ...success, data: dataId });
+		return typeof dataId === 'undefined' ? res.status(404).json({ ...error, message: 'Data not found!' }) : res.status(200).json({ ...success, data: dataId });
 	}
 
 	if (new Date(identifier) instanceof Date && !isNaN(new Date(identifier))) {
 		const dataDate = getByDate(identifier, kecamatan);
-		return typeof dataDate === "undefined" ? res.status(404).json({ ...error, message: "Data not found!" }) : res.status(200).json({ ...success, data: dataDate });
+		return typeof dataDate === 'undefined' ? res.status(404).json({ ...error, message: 'Data not found!' }) : res.status(200).json({ ...success, data: dataDate });
 	}
 
 	return res.status(404).json({
@@ -60,4 +72,4 @@ const CustomIdData = (req, res) => {
 	});
 };
 
-module.exports = { HomeController, ErrorController, DataController, DataKecamatanController, CustomIdData };
+module.exports = { HomeController, ErrorController, DataController, DataKecamatanController, CustomIdData, ListKecamatanController };
--- server/routes/DataRouter.js
+++ server/routes/DataRouter.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const middleware = require("../middleware");
-const { DataController, DataKecamatanController, CustomIdData } = require("../controller");
+const middleware = require('../middleware');
+const { DataController, DataKecamatanController, CustomIdData, ListKecamatanController } = require('../controller');
 
-router.get("/", middleware, DataController);
-router.get("/kecamatan", middleware, DataKecamatanController);
-router.get("/kecamatan/:identifier", middleware, DataKecamatanController);
-router.get("/:identifier", middleware, CustomIdData);
-router.get("/:identifier/:kecamatan", middleware, CustomIdData);
+router.get('/', middleware, DataController);
+router.get('/kecamatan', middleware, DataKecamatanController);
+router.get('/kecamatan/list', middleware, ListKecamatanController);
+router.get('/kecamatan/:identifier', middleware, DataKecamatanController);
+router.get('/:identifier', middleware, CustomIdData);
+router.get('/:identifier/:kecamatan', middleware, CustomIdData);
 
 module.exports = router;
