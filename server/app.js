845c3ee7854ec6f6434c5406895665a374119406
rzkytmgr <rzkytmgr@gmail.com>
2021-09-30 17:08:18 +0700

    :gear: CORS Access Control Origin

--- server/app.js
+++ server/app.js
@@ -6,6 +6,10 @@ const DataRouter = require('./routes/DataRouter');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+});
+
 app.use('/data', DataRouter);
 app.use('/', IndexRouter);
 
